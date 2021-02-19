b89547cbe0a00a8bc0fe05637cb117e9f8c4181e
shutotakizawa <md.takizawa@gmail.com>
2021-02-19 12:47:20 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:15.9.0-alpine3.11
+
+WORKDIR /usr/src/app
---
+++ sample/src/Parent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Child from './Child'
+
+// ‰º‚Í‚ ‚­‚Ü‚ÅJSX‚Ì‘‚«•û
+// ÀÛ‚É‚ÍATypeScript‚æ‚¤‚É‘‚«’¼‚·
+function Parent () {
+    return (
+        <div>
+            Parent
+            <Child />
+        </div>
+    )
+}
+
+export default Parent
